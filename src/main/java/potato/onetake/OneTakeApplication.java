43f8bc6f95314e4fc8c6b2ebf2656049beea40d7
HyWChoi <chw2309@icloud.com>
2024-09-06 19:38:43 +0900

    chore: 프로젝트 초기 세팅 리뉴얼 by hyeonwch

---
+++ src/main/java/potato/onetake/OneTakeApplication.java
@@ -0,0 +1,13 @@
+package potato.onetake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneTakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneTakeApplication.class, args);
+	}
+
+}
