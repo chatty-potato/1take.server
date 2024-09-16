02c8dc44ab93f5dce64a451feb8784ebd4ffcd7d
HyWChoi <chw2309@icloud.com>
2024-09-16 19:51:00 +0900

    feature: interview Dto 작성

---
+++ src/main/java/potato/onetake/domain/Ineterview/dto/InterviewBeginRequestDto.java
@@ -0,0 +1,8 @@
+package potato.onetake.domain.Ineterview.dto;
+
+import java.util.List;
+
+public class InterviewBeginRequestDto {
+	private String title;
+	private List<String> categories;
+}
---
+++ src/main/java/potato/onetake/domain/Ineterview/dto/InterviewListResponseDto.java
@@ -0,0 +1,24 @@
+package potato.onetake.domain.Ineterview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+public class InterviewListResponseDto {
+	private List<InterviewSessionDto> interviewSessions;
+
+	@Getter
+	@Setter
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class InterviewSessionDto{
+		private Long sessionID;
+		private String title;
+		private String date;
+		private Integer score;
+		private Boolean done;
+	}
+}
