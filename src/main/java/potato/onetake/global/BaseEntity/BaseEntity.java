6140d891083579a4051d013ed6495254318d99e0
HyWChoi <chw2309@icloud.com>
2024-09-16 13:50:51 +0900

    feature: BaseEntity 작

--- src/main/java/potato/onetake/global/BaseEntity/BaseEntity.java
+++ src/main/java/potato/onetake/global/BaseEntity/BaseEntity.java
@@ -1,4 +1,26 @@
 package potato.onetake.global.BaseEntity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@CreatedDate
+	private Date createdDate;
+
+	@LastModifiedDate
+	private Date lastModifiedDate;
+
+	private Date deletedDate;
 }
