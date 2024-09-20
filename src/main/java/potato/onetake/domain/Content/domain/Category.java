af1e76fac13c1c2445e667948155882f2d7a14cd
HyWChoi <chw2309@icloud.com>
2024-09-20 10:13:09 +0900

    :sparkles: Question, Category Entity 작성

--- src/main/java/potato/onetake/domain/Content/domain/Category.java
+++ src/main/java/potato/onetake/domain/Content/domain/Category.java
@@ -1,4 +1,21 @@
 package potato.onetake.domain.Content.domain;
 
-public class Category {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import potato.onetake.global.BaseEntity.BaseEntity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="category")
+public class Category extends BaseEntity {
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
 }
--- src/main/java/potato/onetake/domain/Content/domain/Question.java
+++ src/main/java/potato/onetake/domain/Content/domain/Question.java
@@ -1,6 +1,21 @@
 package potato.onetake.domain.Content.domain;
 
-import potato.onetake.domain.Content.dao.QuestionRepository;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import potato.onetake.global.BaseEntity.BaseEntity;
 
-public class Question implements QuestionRepository {
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="Question")
+public class Question extends BaseEntity {
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
 }
