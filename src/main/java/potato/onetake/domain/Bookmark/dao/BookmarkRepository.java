0c281e2b60c9f099478f746e111caa60e2761fef
HyWChoi <chw2309@icloud.com>
2024-09-20 13:59:45 +0900

    :sparkles: Bookmark Entity 생성 및 Repository에 적용

--- src/main/java/potato/onetake/domain/Bookmark/dao/BookmarkRepository.java
+++ src/main/java/potato/onetake/domain/Bookmark/dao/BookmarkRepository.java
@@ -1,4 +1,9 @@
 package potato.onetake.domain.Bookmark.dao;
 
-public interface BookmarkRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import potato.onetake.domain.Bookmark.domain.Bookmark;
+
+@Repository
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 }
--- src/main/java/potato/onetake/domain/Bookmark/domain/Bookmark.java
+++ src/main/java/potato/onetake/domain/Bookmark/domain/Bookmark.java
@@ -1,4 +1,33 @@
 package potato.onetake.domain.Bookmark.domain;
 
-public class Bookmark {
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import potato.onetake.domain.Member.domain.Profile;
+import potato.onetake.domain.Interview.domain.InterviewQna;
+import potato.onetake.global.BaseEntity.BaseEntity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "bookmark")
+public class Bookmark extends BaseEntity {
+
+	@ManyToOne()
+	@JoinColumn(name = "profile_id", referencedColumnName = "id")
+	private Profile profile;
+
+	@ManyToOne()
+	@JoinColumn(name = "qna_id", referencedColumnName = "id")
+	private InterviewQna qna;
+
+	public Bookmark(final Profile profile, final InterviewQna qna){
+		this.profile = profile;
+		this.qna = qna;
+	}
 }
