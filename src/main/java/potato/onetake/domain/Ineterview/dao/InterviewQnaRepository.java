0983ef26cf54c914f06d71c17837e46b533c0f18
HyWChoi <chw2309@icloud.com>
2024-09-24 20:45:30 +0900

    :sparkles: interview service 작성

--- src/main/java/potato/onetake/domain/Ineterview/dao/InterviewQnaRepository.java
+++ src/main/java/potato/onetake/domain/Ineterview/dao/InterviewQnaRepository.java
@@ -4,6 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import potato.onetake.domain.Ineterview.domain.InterviewQna;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InterviewQnaRepository extends JpaRepository<InterviewQna, Long> {
+	Optional<InterviewQna> findByInterviewIdAndQuestionId(Long interviewId, Long questionId);
+
+	List<InterviewQna> findAllByInterviewId(Long interviewId);
+
+	Optional<Long> findQuestionIdByInterviewId(Long interviewId);
 }
