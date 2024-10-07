9ca45c7c2cef8f542f44ac3e075de3e611b168f9
gladhee <gladhee@icloud.com>
2024-10-07 16:29:31 +0900

    :sparkles: Completing operations related to handlers

--- src/main/java/potato/onetake/infrastructure/auth/CustomOauth2User.java
+++ src/main/java/potato/onetake/infrastructure/auth/CustomOauth2User.java
@@ -1,16 +1,18 @@
 package potato.onetake.infrastructure.auth;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 
+import java.util.Collection;
+import java.util.Map;
+
 public class CustomOauth2User extends DefaultOAuth2User {
+
 	private final String uuid;
 
-	public CustomOauth2User(DefaultOAuth2User defaultOAuth2User, String uuid) {
-		super(defaultOAuth2User.getAuthorities(), defaultOAuth2User.getAttributes(), defaultOAuth2User.getName());
+	public CustomOauth2User(Collection<? extends GrantedAuthority> authorities, Map<String, Object> attributes, String nameAttributeKey, String uuid) {
+		super(authorities, attributes, nameAttributeKey);
 		this.uuid = uuid;
 	}
 
-	public String getUuid() {
-		return uuid;
-	}
 }
