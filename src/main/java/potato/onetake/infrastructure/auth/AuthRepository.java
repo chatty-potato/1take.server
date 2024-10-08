e74ce88bf95c036a935b899f8af14da6d235c157
gladhee <gladhee@icloud.com>
2024-10-08 15:28:27 +0900

    :sparkles: add findByUuid in AuthRepository

--- src/main/java/potato/onetake/infrastructure/auth/AuthRepository.java
+++ src/main/java/potato/onetake/infrastructure/auth/AuthRepository.java
@@ -1,9 +1,11 @@
 package potato.onetake.infrastructure.auth;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthRepository extends JpaRepository<Auth, Long> {
 	Optional<Auth> findByProviderAndProviderId(String provider, String providerId);
+
+	Optional<Auth> findByUuid(String uuid);
 }
